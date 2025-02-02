95f64c63a6405c854b6a847c19f31a8a2d7bcb46
Chris Jensen <chris@speakeasy.local>
2025-02-02 23:14:16 +1300

    Change repost to share icon

--- src/components/icons/Repost.tsx
+++ src/components/icons/Repost.tsx
@@ -1,13 +1,38 @@
-import {createSinglePathSVG} from './TEMPLATE'
+import React from 'react'
+import {G, Path, Svg} from 'react-native-svg'
 
-export const Repost_Stroke2_Corner0_Rounded = createSinglePathSVG({
-  path: 'M16.293 2.293a1 1 0 0 1 1.414 0l3 3a1 1 0 0 1 0 1.414l-3 3a1 1 0 0 1-1.414-1.414L17.586 7H5v4a1 1 0 1 1-2 0V6a1 1 0 0 1 1-1h13.586l-1.293-1.293a1 1 0 0 1 0-1.414ZM21 13v5a1 1 0 0 1-1 1H6.414l1.293 1.293a1 1 0 1 1-1.414 1.414l-3-3a1 1 0 0 1 0-1.414l3-3a1 1 0 0 1 1.414 1.414L6.414 17H19v-4a1 1 0 1 1 2 0Z',
-})
+import {Props, useCommonSVGProps} from './common'
 
-export const Repost_Stroke2_Corner2_Rounded = createSinglePathSVG({
-  path: 'M17.957 2.293a1 1 0 1 0-1.414 1.414L17.836 5H6a3 3 0 0 0-3 3v3a1 1 0 1 0 2 0V8a1 1 0 0 1 1-1h11.836l-1.293 1.293a1 1 0 0 0 1.414 1.414l2.47-2.47a1.75 1.75 0 0 0 0-2.474l-2.47-2.47ZM20 12a1 1 0 0 1 1 1v3a3 3 0 0 1-3 3H6.164l1.293 1.293a1 1 0 1 1-1.414 1.414l-2.47-2.47a1.75 1.75 0 0 1 0-2.474l2.47-2.47a1 1 0 0 1 1.414 1.414L6.164 17H18a1 1 0 0 0 1-1v-3a1 1 0 0 1 1-1Z',
-})
+export const Repost_Stroke2_Corner2_Rounded = React.forwardRef<Svg, Props>(
+  function RepostIcon(props, ref) {
+    const {size, style, ...rest} = useCommonSVGProps(props)
 
-export const Repost_Stroke2_Corner3_Rounded = createSinglePathSVG({
-  path: 'M16.793 2.293a1 1 0 0 1 1.414 0L20.5 4.586a2 2 0 0 1 0 2.828l-2.293 2.293a1 1 0 0 1-1.414-1.414L18.086 7H7a2 2 0 0 0-2 2v2a1 1 0 1 1-2 0V9a4 4 0 0 1 4-4h11.086l-1.293-1.293a1 1 0 0 1 0-1.414ZM20 12a1 1 0 0 1 1 1v2a4 4 0 0 1-4 4H5.914l1.293 1.293a1 1 0 1 1-1.414 1.414L3.5 19.414a2 2 0 0 1 0-2.828l2.293-2.293a1 1 0 0 1 1.414 1.414L5.914 17H17a2 2 0 0 0 2-2v-2a1 1 0 0 1 1-1Z',
-})
+    return (
+      <Svg
+        {...rest}
+        ref={ref}
+        viewBox="-50 0 1000.000000 831.000000"
+        width={size}
+        height={size}
+        style={style}>
+        <G
+          transform="translate(0.000000,831.000000) scale(0.100000,-0.100000)"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="600">
+          <Path
+            d="M4934 8282 c-101 -36 -175 -118 -216 -242 -22 -64 -22 -73 -26 -834
+l-3 -769 -192 7 c-1028 36 -2016 -292 -2825 -938 -558 -445 -1017 -1051 -1303
+-1718 -281 -656 -407 -1396 -359 -2098 28 -394 91 -730 213 -1131 25 -79 71
+-231 103 -337 31 -106 60 -189 64 -185 3 4 32 96 64 203 181 612 240 780 365
+1042 567 1194 1680 2020 2966 2202 200 29 327 36 617 36 l287 0 3 -732 c4
+-723 4 -734 26 -798 42 -126 125 -215 230 -246 103 -32 240 -6 343 63 46 31
+1820 1447 3217 2568 361 289 409 337 452 452 12 32 27 89 33 126 22 141 -32
+303 -139 415 -32 34 -844 690 -1804 1457 -1403 1123 -1760 1403 -1820 1432
+-65 32 -86 37 -160 40 -63 3 -98 -1 -136 -15z"
+          />
+        </G>
+      </Svg>
+    )
+  },
+)
