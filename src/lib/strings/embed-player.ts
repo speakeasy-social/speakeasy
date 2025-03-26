6f44e963f4d0a6427c8fce7561287ea2951c1932
jpm-nz <32984946+jpm-nz@users.noreply.github.com>
2025-03-27 00:10:52 +1300

    Revert "change host in embed player"
    This reverts commit 01706f1ef204409cb033af4fedc41e4cc90fc538.

--- src/lib/strings/embed-player.ts
+++ src/lib/strings/embed-player.ts
@@ -9,10 +9,10 @@ const IFRAME_HOST = isWeb
   ? // @ts-ignore only for web
     window.location.host === 'localhost:8100'
     ? 'http://localhost:8100'
-    : 'https://magnetic.spkeasy.social'
+    : 'https://bsky.app'
   : __DEV__ && !process.env.JEST_WORKER_ID
   ? 'http://localhost:8100'
-  : 'https://magnetic.spkeasy.social'
+  : 'https://bsky.app'
 
 export const embedPlayerSources = [
   'youtube',
