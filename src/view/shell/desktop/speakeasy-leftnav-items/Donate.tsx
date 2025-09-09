c53fd86b10ad27ce1e1aa72a27efd61030a8ebe6
Hazel Garcia <hazelpetagarcia@gmail.com>
2025-09-09 17:46:59 +1000

    Undo extracting LeftNav items into separate components

--- src/view/shell/desktop/speakeasy-leftnav-items/Donate.tsx
+++ src/view/shell/desktop/speakeasy-leftnav-items/Donate.tsx
@@ -3,8 +3,7 @@ import {useLingui} from '@lingui/react'
 
 import {usePalette} from '#/lib/hooks/usePalette'
 import {Growth_Stroke2_Corner0_Rounded as Growth} from '#/components/icons/Growth'
-import {NAV_ICON_WIDTH} from '../constants'
-import NavItem from '../NavItem'
+import {NAV_ICON_WIDTH, NavItem} from '../LeftNav'
 
 const Donate = () => {
   const {_} = useLingui()
