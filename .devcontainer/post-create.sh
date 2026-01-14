fe0e8acda08742fcdda089064cb47c211205e3f3
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2026-01-14 07:16:28 +0000

    Improve startup for codespaces and guidance

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,18 +1,55 @@
 #!/bin/bash
 set -eux
 
+# FAIL:
+# This doesn't work in code spaces because every port is forwarded on a unique
+# hostname (why codespaces? why?) which causes CORS errors.
+# 
+# To make this work, we'd need to configure CORS in ATproto and the services
+# to permit *.app.github.dev domains
+
+echo 🚀 Starting Speakeasy playground (this may take a while)
+echo
+echo
+
+echo
+echo 🔧 Building ATproto mock environment
+echo
+
 # Clone and setup atproto
-git clone https://github.com/bluesky-social/atproto.git /workspaces/atproto
-cd /workspaces/atproto
+git clone https://github.com/bluesky-social/atproto.git ../atproto
+cd ../atproto
 make deps
-make run-dev-env &
+pnpm build
+
+echo
+echo 🔧 Building Speakeasy services
+echo
 
 # Clone and setup services
-git clone https://github.com/speakeasy-social/services.git /workspaces/services 
-cd /workspaces/services
+git clone https://github.com/speakeasy-social/services.git ../services 
+cd ../services
 pnpm install
+pnpm build
+pnpm run dev:setup
+
+echo
+echo 🚀 Launching ATproto
+echo
+
+cd ../atproto
+make run-dev-env &
+
+echo
+echo 🚀 Launching Speakeasy Services 🤫
+echo
+
+cd ../services
 pnpm run dev &
 
-# Install main repo dependencies (if any)
-cd /workspaces/speakeasy
-pnpm install  # Add your actual dependency setup here
+echo
+echo 🚀 Launching Speakeasy Web 🖥️
+echo
+
+cd ../speakeasy
+yarn web:dev
---
+++ .devcontainer/welcome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat << EOF
+Welcome to Speakeasy!
+
+To start using, install the necessary services by opening a terminal and running:
+  yarn
+and, then run
+  yarn web:dev
+
+Both of these are a bit slow, they may take up to 1 minute each.
+EOF
