c6771396c4d7c38fef9d4ec4140198ce7f6e15e5
Chris Jensen <chris@speakeasy.local>
2026-02-07 23:37:23 +1100

    Supporters: add Donate and Volunteer CTAs

--- src/view/screens/Supporters.tsx
+++ src/view/screens/Supporters.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {ListRenderItemInfo, View} from 'react-native'
-import {useFocusEffect} from '@react-navigation/native'
+import {useFocusEffect, useLinkProps} from '@react-navigation/native'
 
+import {useOpenLink} from '#/lib/hooks/useOpenLink'
 import {CommonNavigatorParams, NativeStackScreenProps} from '#/lib/routes/types'
 import {useTestimonialsWithProfiles} from '#/state/queries/testimonials'
 import {useSession} from '#/state/session'
 import {useSetMinimalShellMode} from '#/state/shell'
 import {atoms as a, useTheme} from '#/alf'
+import {Button, ButtonText} from '#/components/Button'
 import * as Layout from '#/components/Layout'
 import {ListMaybePlaceholder} from '#/components/Lists'
 import {Text} from '#/components/Typography'
+import {FEED_PAUSE_CTAS} from '#/constants/pause-feed-cta'
 import {TestimonialItem} from '../com/supporters/TestimonialItem'
 import {Testimonial} from '../com/supporters/types'
 import {List} from '../com/util/List'
@@ -20,6 +23,10 @@ export function SupportersScreen(_props: Props) {
   const setMinimalShellMode = useSetMinimalShellMode()
   const t = useTheme()
   const {currentAccount} = useSession()
+  const {onPress: onDonatePress} = useLinkProps({to: '/donate'})
+  const openLink = useOpenLink()
+  const discordUrl =
+    FEED_PAUSE_CTAS.find(cta => cta.id === 'discord')?.url ?? ''
 
   const {
     data: testimonials,
@@ -51,9 +58,27 @@ export function SupportersScreen(_props: Props) {
           Speakeasy's vision of social media is made possible by the generous
           support of our community
         </Text>
+        <View style={[a.flex_row, a.gap_sm, a.mt_md, a.justify_center]}>
+          <Button
+            size="small"
+            variant="outline"
+            color="primary"
+            label="Donate"
+            onPress={onDonatePress}>
+            <ButtonText>Donate</ButtonText>
+          </Button>
+          <Button
+            size="small"
+            variant="outline"
+            color="primary"
+            label="Volunteer"
+            onPress={() => openLink(discordUrl)}>
+            <ButtonText>Volunteer</ButtonText>
+          </Button>
+        </View>
       </View>
     ),
-    [t.atoms.border_contrast_low],
+    [t.atoms.border_contrast_low, onDonatePress, openLink, discordUrl],
   )
 
   const onRefresh = React.useCallback(async () => {
